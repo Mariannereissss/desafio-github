f4a1beb8902e5fc10f56215b4f6323587b347f10
Mari <dmariannereis@gmail.com>
2025-06-04 03:38:26 +0000

    desafio

---
+++ concatenando.py
@@ -0,0 +1,5 @@
+informacao1 = input ("Digite a primeira informação: ")
+informacao2 = input ("Digite a segunda informação: ")
+
+informacaoConcatenada = informacao1 + " " + informacao2
+print ("A informação concatenada é: " , informacaoConcatenada)
---
+++ repetindoTextos.py
@@ -0,0 +1,4 @@
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((texto + ' ' )* numero)
